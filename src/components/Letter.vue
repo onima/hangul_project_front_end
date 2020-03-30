c5f8d6097523ad52d0ad9ee2257430ca0695a251
Alexis POirier <alexispoirier@live.com>
2020-03-30 14:48:40 +0200

    First commit

---
+++ src/components/Letter.vue
@@ -0,0 +1,30 @@
+<template>
+  <img class="letter" alt="Letter" :src="imageSrc" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Letter',
+  props: {
+    name: String
+  },
+  computed: {
+    imageSrc () {
+      const images = require.context('../assets/', false, /\.png$/)
+      return images('./' + this.name + '.png')
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .letter {
+    position: relative;
+    z-index: 1;
+    right: 700px;
+    bottom: 500px;
+  }
+</style>
---
+++ src/components/PlayGround.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="playground"/>
+    <Letter name="a"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Letter from '@/components/Letter.vue'
+
+export default Vue.extend({
+  name: 'PlayGround',
+  components: {
+    Letter
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .playground {
+    width: 1200px;
+    height: 800px;
+    opacity: 0.3;
+    display: inline-block;
+    background-repeat: no-repeat;
+    background-image: url("../assets/south_korea_flag.png");
+  }
+</style>
