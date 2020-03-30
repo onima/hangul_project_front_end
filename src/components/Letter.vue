196e942a6726484f7cf398eb944fca0d74a824ab
Alexis POirier <alexispoirier@live.com>
2020-03-30 15:45:21 +0200

    Make the letter moving on the y axis

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="letter" alt="Letter" :src="imageSrc" />
+  <img class="letter" alt="Letter" :src="imageSrc" :style="{ top: letterPosition}"/>
 </template>
 
 <script lang="ts">
@@ -8,12 +8,16 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'Letter',
   props: {
-    name: String
+    name: String,
+    y: Number
   },
   computed: {
     imageSrc () {
       const images = require.context('../assets/', false, /\.png$/)
       return images('./' + this.name + '.png')
+    },
+    letterPosition () {
+      return `${this.y}px`
     }
   }
 })
@@ -25,6 +29,5 @@ export default Vue.extend({
     position: relative;
     z-index: 1;
     right: 700px;
-    bottom: 500px;
   }
 </style>
