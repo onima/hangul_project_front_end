6d49f44539128b5b9180ccfe5e40e6b74469ecfe
Alexis POirier <alexispoirier@live.com>
2020-04-02 15:42:52 +0200

    Create start page

---
+++ src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+  <PlayGround/>
+</template>
+
+<script>
+// @ is an alias to /src
+import PlayGround from '@/components/PlayGround.vue'
+
+export default {
+  name: 'Game',
+  components: {
+    PlayGround
+  }
+}
+</script>
